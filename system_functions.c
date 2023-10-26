f071cdc9aca46107fe263ed80bd070f8eef4c735
Javier20060904 <93936175+Javier20060904@users.noreply.github.com>
2023-10-25 19:21:59 -0600

    Comentar codigo y ultimos retoques

--- system_functions.c
+++ system_functions.c
@@ -7,11 +7,11 @@
 #include "system.h"
 #define SEGUNDO __SYSTEM_CLOCK
 
-uint8_t contadorEstado = 0x00;
-uint8_t contadorApago = 0x00;
-uint8_t contadorSeg = 0x00;
-bool estado = 0; //0 = Verde, 1 = Rojo
-bool prendido = 1;
+uint8_t contadorEstado = 0x00; //Contador de cuantas veces se pone en pausa
+uint8_t contadorApago = 0x00; //Contador de cuantas veces se presiona el boton de apago
+uint8_t contadorSeg = 0x00; //Contador de segundos (para el timer)
+bool estado = 0; //0 = Verde (En ejecucion), 1 = Rojo (Pausa)
+bool prendido = 1; //Estado del LED RGB
 
 void cambioDeColor(void);
 void Timer32_INT1(void);
@@ -51,6 +51,7 @@ void System_InicialiceUART (void)
 
 void funcion_inicial (void)
 {
+    //Reinicio de las variables para otro ciclo
     prendido = 1;
     contadorEstado = 0x00;
     contadorApago = 0x00;
@@ -78,18 +79,22 @@ void funcion_inicial (void)
 
 void process_events(void)
 {
-    //UART_putsf(MAIN_UART, "Programa ejecutandose.\r\n");
+    if(estado){ //Sistema en pausa
+        return;
+    }
+    UART_putsf(MAIN_UART, "Programa ejecutandose.\r\n");
 
     //Switch de PAUSA
     if(GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON1) != BOARD_BUTTON_NORMAL_STATE)
     {
-        contadorEstado++;
+        contadorEstado++; //Incrementa el contador de estado
         //Asigna al estado lo mismo que la condicional if, o sea, un 1
         estado = (GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON1) != BOARD_BUTTON_NORMAL_STATE);
-        //Si el contador es igual a 0 entonces la variable tambien
+        //Si el contador es igual a 6 entonces se apaga el led
         prendido = prendido * !(contadorEstado == 0x06);
+        cambioDeColor(); //Cambia de color el RGB
         if(contadorEstado < 0x06)
-            UART_putsf(MAIN_UART, "Pausa \r\n");
+            UART_putsf(MAIN_UART, "Pausa. \r\n");
         if(contadorEstado == 0x06){
             UART_putsf(MAIN_UART, "Programa terminado. \r\n");
             EUSCI_A_CMSIS(MAIN_UART) -> IFG &= !0x0D;       //Borra la bandera UCRXIFG
@@ -100,22 +105,30 @@ void process_events(void)
     //Switch de APAGADO
     if(GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON2) != BOARD_BUTTON_NORMAL_STATE)
     {
-        contadorApago++;
-        prendido = prendido * !(contadorApago & 0x02);
-        cambioDeColor();
+        contadorApago++; //Incrementa el contador de apagado
+        prendido = prendido * !(contadorApago & 0x02); //Si el contador es igual a dos entonces el estado de prendido es igual a 0
+        cambioDeColor(); //Cambia de color el RGB
         if(contadorApago & 0x02){
             UART_putsf(MAIN_UART, "Programa terminado. \r\n");
             EUSCI_A_CMSIS(MAIN_UART) -> IFG &= !0x0D;       //Borra la bandera UCRXIFG
         }
         if(contadorApago & 0x01)
             UART_putsf(MAIN_UART, "Presione de nuevo para apagar. \r\n");
-        //Reinicia el contador de APAGADO
+        //Reinicia el contador de apagado cuando llegue a 2
         contadorApago = contadorApago * !(contadorApago & 0x02);
         while(GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON2) != BOARD_BUTTON_NORMAL_STATE);
     }
 
 
 }
+
+/*FUNCTION******************************************************************************
+*
+* Function Name    : Timer32_INT1
+* Returned Value   : None.
+* Comments         :
+*
+*END***********************************************************************************/
 
 void Timer32_INT1(void)
 {
@@ -127,7 +140,7 @@ void Timer32_INT1(void)
 
     //Si el estado esta en 0 y el contador de apago tiene un 1 en su cuenta, entonces suma uno
     contadorSeg += ((0x01) * !estado * (contadorApago == 0x01));
-    //Si esta en el rango deseado el valor de la variable queda igual
+    //Si contadorSeg esta en el rango (0-3 seg), el valor de la variable queda igual, de caso contario se pone a 0
     contadorApago = contadorApago * !estado * !(contadorSeg == 0x03);
     //Si el contador a hecho una suma de 3 segundos entonces vuelve a 0
     contadorSeg = contadorSeg * !estado * !(contadorSeg == 0x03);
@@ -136,6 +149,13 @@ void Timer32_INT1(void)
     estado = (estado * !estado);
 }
 
+/*FUNCTION******************************************************************************
+*
+* Function Name    : cambioDeColor
+* Returned Value   : None.
+* Comments         : cambia el color segun los estados de las variables
+*
+*END***********************************************************************************/
 void cambioDeColor(void){
     //Varias condicionales en el valor que se le va enviar
     //si el contadorApago es igual a 1
